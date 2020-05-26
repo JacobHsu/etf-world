aaa3ed250fc3eab6a23c28327d3c70e02b2ed8f1
jacobhsu <jacob.hsu.tw@gmail.com>
2020-05-26 17:15:21 +0800

    vue-datamaps bubblesConfig

--- README.md
+++ README.md
@@ -1,23 +1,32 @@
 # etf-world
 
-[Pure.CSS快速指南](http://www.vue5.com/purecss/purecss_quick_guide.html)
+[Pure.CSS 快速指南](http://www.vue5.com/purecss/purecss_quick_guide.html)
+
+```JS
+  dataUrl:
+          'https://raw.githubusercontent.com/Seungwoo321/vue-datamaps/master/demo/example-vue-cli3/public/data/world.json',
+```
 
 ## Project setup
+
 ```
 yarn install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 yarn run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 yarn run build
 ```
 
 ### Lints and fixes files
+
 ```
 yarn run lint
 ```
--- src/components/DatamapsBasic.vue
+++ src/components/DatamapsBasic.vue
@@ -1,36 +1,95 @@
 <template>
   <div>
     <!-- <h4>Basic:</h4> -->
-    <vue-datamaps :geographyConfig="geographyConfig"/>
+    <vue-datamaps
+      :geographyConfig="geographyConfig"
+      :bubblesConfig="bubblesConfig"
+      :data="data"
+      :fills="fills"
+      @custom:popup-bubble="popupTemplate"
+      bubbles
+     
+    >
+     <!-- labels -->
+      <div slot="hoverBubbleInfo" class="hoverinfo" style="text-align:center;">
+        <b>Name</b>: {{ popupData.name }}<br />
+        <b>Yield</b>: {{ popupData.yeild }}<br />
+        Exploded on {{ popupData.date }} by the {{ popupData.country }}
+      </div>
+    </vue-datamaps>
     <!-- <vue-datamaps/> -->
   </div>
 </template>
 
 <script>
+import bubblesData from '../data/bubbles.json'
 import { VueDatamaps } from 'vue-datamaps'
+
 export default {
   components: {
-    VueDatamaps
+    VueDatamaps,
   },
-  data () {
+  data() {
     return {
+      labelsConfig: {
+        fontSize: 14,
+        fontFamily: 'Verdana',
+        labelColor: '#fff',
+        lineWidth: 1,
+      },
       geographyConfig: {
         borderColor: '#DEDEDE',
-        borderWidth: 0.5,
+        borderWidth: 0.75,
         highlightBorderWidth: 2,
-        dataUrl: 'https://raw.githubusercontent.com/Seungwoo321/vue-datamaps/master/demo/example-vue-cli3/public/data/world.json'
-      }
+        highlightBorderOpacity: 1,
+        dataUrl:'world.json',
+      },
+      data: {
+        'USA': { fillKey: 'active' }
+      },
+      fills: {
+        defaultFill: '#ABDDA4',
+        USA: 'blue',
+        RUS: 'red',
+        active: 'rgb(113, 142, 179)'
+      },
+      bubblesConfig: {
+        popupTemplate: true,
+        data: bubblesData.data,
+      },
+      popupData: {
+        name: '',
+        yeild: '',
+        date: '',
+        country: '',
+      },
     }
-  }
+  },
+  created() {
+
+  },
+  updated() {
+
+  },
+  methods: {
+    popupTemplate({ datum }) {
+      this.popupData = {
+        name: datum.name,
+        yeild: datum.yeild,
+        date: datum.date,
+        country: datum.country,
+      }
+    },
+  },
 }
 </script>
 
 <style>
 .map {
-    height: 0;
-    width: 100%;
-    padding-top: 45%;
-    position: relative;
-    /* border:1px dotted blue;  */
+  height: 0;
+  width: 100%;
+  padding-top: 45%;
+  position: relative;
+  /* border:1px dotted blue;  */
 }
 </style>
