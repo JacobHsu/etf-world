5a827a12551532b9997345a056733c3c45660d13
Jacob Hsu <jacob.hsu.tw@gmail.com>
2020-08-18 03:11:28 +0800

    EWT, THD, TUR, UAE, EWU, VOO, VNM

--- src/components/DatamapsBasic.vue
+++ src/components/DatamapsBasic.vue
@@ -88,7 +88,7 @@ export default {
     etfsData(res) {
       const {ARGT, EWA, EWO, EWK, EWZ, EWC, ECH, MCHI, ICOL, EDEN, EGPT, EFNL, EWQ, EWG, GREK, EWH, INDA, EIDO,
       EIRL, EIS, EWI, EWJ, EWY, EWM, EWW, EWN, ENZL, NGE, ENOR, PAK, EPU, EPHE, EPOL, PGAL, QAT, RSX,
-      KSA, EWS, EZA, EWP, EWD, EWL } = res.data.etf
+      KSA, EWS, EZA, EWP, EWD, EWL, EWT, THD, TUR, UAE, EWU, VOO, VNM} = res.data.etf
       const etfs = {
         ARG:ARGT.periodChg, AUS:EWA.periodChg, AUT:EWO.periodChg, 
         BEL:EWK.periodChg, BRA:EWZ.periodChg, CAN:EWC.periodChg,
@@ -103,7 +103,10 @@ export default {
         PER:EPU.periodChg, PHL:EPHE.periodChg, POL:EPOL.periodChg,
         PRT:PGAL.periodChg, QAT:QAT.periodChg, RUS:RSX.periodChg,
         SAU:KSA.periodChg, SGP:EWS.periodChg, ZAF:EZA.periodChg,
-        ESP:EWP.periodChg, SWE:EWD.periodChg, CHE:EWL.periodChg
+        ESP:EWP.periodChg, SWE:EWD.periodChg, CHE:EWL.periodChg,
+        TWN:EWT.periodChg, THA:THD.periodChg, TUR:TUR.periodChg,
+        ARE:UAE.periodChg, GBR:EWU.periodChg, USA:VOO.periodChg,
+        VNM:VNM.periodChg
       }
       console.log(etfs) 
 
