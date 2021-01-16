06051462598d2c45a51a616cb86a4027278f8914
jacobhsu <jacob.hsu.tw@gmail.com>
2021-01-16 13:47:32 +0800

    fix vue-datamaps data/world.json

--- src/components/DatamapsBasic.vue
+++ src/components/DatamapsBasic.vue
@@ -42,8 +42,7 @@ export default {
         borderWidth: 2,
         highlightFillColor: '#1C977A',
         popupOnHover: false,
-        highlightOnHover: false,
-        dataUrl:'world.json',
+        highlightOnHover: false
       },
       data: {
         'blue': { fillKey: 'active' }
